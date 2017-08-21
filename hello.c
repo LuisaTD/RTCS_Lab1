c175542b2d82cdd71aa18d16bab2600d1a8f533c
Unknown <luisa.tapiad@gmail.com>
2017-08-21 15:50:13 -0600

    Cambio 1

--- hello.c
+++ hello.c
@@ -7,5 +7,6 @@ void main()
 	printf("Codigo Development 2 intento 1");
 	printf("Codigo Development 2 intento 2"); 
 	printf("Codigo Development 3"); 
+	printf("Codigo Development 3 intento 1"); 
 	return 0;
 }
